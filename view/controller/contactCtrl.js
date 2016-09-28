b14bcaf5c4c9fcfc0dee4cebfdb0c3ac40a2ddda
nextitsoftsys <yadav.jayprakash20@yahoo.in>
2016-09-28 17:37:41 +0530

    updated

--- view/controller/contactCtrl.js
+++ view/controller/contactCtrl.js
@@ -1,6 +1,6 @@
 
-angular.module('bridgeApp').controller('contactCtrl', function ($scope, $http, $templateCache) {
-    $scope.codeStatus = "";
+angular.module('bridgeApp').controller('contactCtrl', function ($scope, $http) {
+    
     $scope.submit = function () {
               var formData = {
                             'name': this.name,
@@ -13,16 +13,14 @@ angular.module('bridgeApp').controller('contactCtrl', function ($scope, $http, $
                 // Accessing the Angular $http Service to send data via REST Communication to Node Server.
                 $http.post('https://bridge941.herokuapp.com/setData', formData).success(function (response) {
                     console.log("success"); // Getting Success Response in Callback
-                    document.userForm.reset();
-                    $scope.codeStatus = response.data;
-                     
+                   document.getElementById("myform1").reset();
+                    
                 }).error(function (response) {
                     console.log("error"); // Getting Error Response in Callback
-                    $scope.codeStatus = response || "Request failed";
-                    console.log($scope.codeStatus);
+                   
                 });
                 // Calling the list function in Angular Controller to show all current data in HTML
-                return false;
+               
 
 
            
